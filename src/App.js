a19356520fc83582d0c7d79c0717f14461a203e1
csai <cheerlachinnikrishnasai@gmail.com>
2022-12-07 20:47:59 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, {  useState } from 'react'
+
+const App = () => {
+
+  const [movie , setMovie] = useState("")
+  const [value , setValue] = useState([])
+ 
+  const fetchData =  async (movie) => {
+    const response = await fetch(`https://www.omdbapi.com/?s=${movie}&&&apikey=e227cea2`)
+    const data = await response.json()
+    setValue(data.Search)
+  }
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    fetchData(movie);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <center className='input'>
+            <form onSubmit={submitHandler}>
+              <input  type="text"
+                      className='inputfield' 
+                      value={movie} 
+                      size ={50}
+                      placeholder="movie..." 
+                      onChange={e => setMovie(e.target.value)} />
+              <br />
+              <input className="search" type="submit" value="Search"/>
+            </form>
+      </center>
+      <br />
+      <div className='All'>
+        { value.map((movie, index) =>{
+          return(
+              <div className='movies'>
+                <center className='movieslist'>
+                    <img src={movie.Poster} alt ={movie.Title} width={150} />
+                    <p>{movie.Title}</p>
+                    <p>{movie.Year}</p>
+                </center>
+              </div>
+          
+          )
+        })}
+       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
